e4882c85921bb43499c55a39b760456c79ad1e15
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-08-17 04:10:38 +0500

    Task5 created and joined with Student Management

---
+++ Task5.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Task5 {
+    public static void main(String[] args) throws IOException {
+        StdManagement stdManagement = new StdManagement();
+    }
+}
