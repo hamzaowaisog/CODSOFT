3e365bb6f652f0973e28ac3b0d59aebb0388b54a
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-08-17 05:18:15 +0500

    Admin page in progress

--- AdmLogin.java
+++ AdmLogin.java
@@ -1,2 +1,150 @@
-public class AdmLogin {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AdmLogin extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        AdmLogin admLogin = new AdmLogin();
+    }
+    JFrame jf;
+    Font f,f1;
+    JButton b1,b2,b3;
+    JLabel l1,l2,l3,l4,l5,l6;
+    ImageIcon img1;
+    JTextField txt; //name
+    JTextField id1; //id
+    JPasswordField pwd; //password
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    String name,pass,checkname,checkpass;
+    int id,checkid;
+    boolean isActive;
+
+
+
+
+    public AdmLogin(){
+        jf = new JFrame();
+        f = new Font("Times New Roman",Font.BOLD,20);//button
+        f1 = new Font("Times New Roman",Font.BOLD,25);//label
+        img1 = new ImageIcon("stdmanagement1.png");
+        jf.setLayout(null);
+
+        l1 = new JLabel();
+        l1.setIcon(img1);
+        l1.setBounds(330,50,700,200);
+        jf.add(l1);
+
+        l2 = new JLabel("Admin Login.");
+        l2.setFont(new Font("Times New Roman",Font.BOLD,40));
+        l2.setForeground(Color.BLACK);
+        l2.setBounds(400,300,500,50);
+        jf.add(l2);
+
+        l3 = new JLabel("Enter Name :");
+        l3.setFont(f1);
+        l3.setForeground(Color.BLACK);
+        l3.setBounds(200,500,350,30);
+        jf.add(l3);
+
+        l4 = new JLabel("Enter Password :");
+        l4.setFont(f1);
+        l4.setForeground(Color.BLACK);
+        l4.setBounds(200,600,350,30);
+        jf.add(l4);
+
+        l5 = new JLabel("Enter ID:");
+        l5.setFont(f1);
+        l5.setForeground(Color.BLACK);
+        l5.setBounds(200,400,350,30);
+        jf.add(l5);
+
+        id1 = new JTextField();
+        id1.setFont(f1);
+        id1.setForeground(Color.BLACK);
+        id1.setBounds(500,400,250,30);
+        id1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if(!(Character.isDigit(c) || (c==KeyEvent.VK_BACK_SPACE) || c==KeyEvent.VK_DELETE)){
+                    e.consume();
+                }
+            }
+        });
+        jf.add(id1);
+
+        txt = new JTextField();
+        txt.setFont(f1);
+        txt.setForeground(Color.BLACK);
+        txt.setBounds(500,500,250,30);
+        jf.add(txt);
+
+        pwd  = new JPasswordField(20);
+        pwd.setFont(f1);
+        pwd.setForeground(Color.BLACK);
+        pwd.setBounds(500,600,250,30);
+        jf.add(pwd);
+
+        b1 = new JButton("Enter",new ImageIcon("ok.png"));
+        b1.setFont(f);
+        b1.setBounds(150,750,175,50);
+        b1.addActionListener(this);
+        jf.add(b1);
+
+        b2 = new JButton("Clear",new ImageIcon("clear.png"));
+        b2.setFont(f);
+        b2.setBounds(435,750,175,50);
+        b2.addActionListener(this);
+        jf.add(b2);
+
+        b3 = new JButton("Exit",new ImageIcon("cancel.png"));
+        b3.setFont(f);
+        b3.setBounds(700,750,175,50);
+        b3.addActionListener(this);
+        jf.add(b3);
+
+        jf.setTitle("Student Management System");
+        jf.setVisible(true);
+        jf.setResizable(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(1000,900);
+        jf.setLocation(220,20);
+        jf.getContentPane().setBackground(new Color(0xcbcbcb));
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == b1){
+            try {
+                if(txt.getText().equals("") || id1.getText().equals("") || pwd.getText().equals("")){
+                    JOptionPane.showMessageDialog(this,"Please Enter the Empty Field");
+                }
+            }
+            catch (Exception ex){
+                ex.printStackTrace();
+            }
+
+
+        }
+        else if (e.getSource() == b2 ){
+            txt.setText("");
+            pwd.setText("");
+            id1.setText("");
+        }
+        else if (e.getSource() == b3){
+            new StdManagement();
+            jf.setVisible(false);
+        }
+
+    }
 }
